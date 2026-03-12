af509040ce92bd913f3699417149fd7ff9b545a8
smolsenbiller <jacob.august.smith@icloud.com>
2026-03-11 20:43:36 -0600

    Added a wand that points towards the mouse.

--- scripts/wizard.gd
+++ scripts/wizard.gd
@@ -5,20 +5,31 @@ const JUMP_VELOCITY = -400.0
 
 @onready var animator : AnimationPlayer = $AnimationPlayer
 @onready var sprite : Sprite2D = $Sprite2D
+@onready var pivot : Node2D = $Pivot
+
 
 func _physics_process(delta: float) -> void:
 	var move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = move_direction * SPEED * delta
 	
+	var mouse_location = get_global_mouse_position()
+	var character_look_direction = get_local_mouse_position()
+	
+	pivot.look_at(mouse_location)
+	
+	
+	#Animation
 	if velocity != Vector2.ZERO:
 		animator.play("walking")
 	else:
 		animator.play("idle")
 	
-	if velocity.x > 0:
+	
+	#Looking direction
+	if character_look_direction.x > 0:
 		sprite.flip_h = false
 	
-	if  velocity.x < 0:
+	if  character_look_direction.x < 0:
 		sprite.flip_h = true
 	
 	move_and_slide()
